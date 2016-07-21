26029e997f7705d0037969b18cde77034d07156d
Dmitry Voytsekhovskiy <v-dmvo@microsoft.com>
2016-07-21 19:18:50 +0300

    Bump version to 0.2.3

--- src/Angara.Table/AssemblyInfo.fs
+++ src/Angara.Table/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Angara.Table")>]
 [<assembly: AssemblyProductAttribute("Angara.Table")>]
 [<assembly: AssemblyDescriptionAttribute("A .NET library to work with plain tables.")>]
-[<assembly: AssemblyVersionAttribute("0.2.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.2")>]
+[<assembly: AssemblyVersionAttribute("0.2.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.2"
-    let [<Literal>] InformationalVersion = "0.2.2"
+    let [<Literal>] Version = "0.2.3"
+    let [<Literal>] InformationalVersion = "0.2.3"
